d1d08b1f7d25450c90247eb1185a31e9317455b6
Lee McGowan <lee.mcgowan.writing@gmail.com>
2024-05-29 23:20:16 +0100

    Added note add functionality. Store notes in a map for easy editing and retrieval.

--- src/lib/scenes/canvas/canvas.tsx
+++ src/lib/scenes/canvas/canvas.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import { styled } from "styled-components";
+import AddButton from "./components/add-button";
 
 const Wrap = styled.section`
   height: 100vh;
   width: 100vw;
   background-color: slateblue;
+  display: flex;
 `;
 
+type Note = {
+  idx: number,
+  text: string,
+}
+
+// Store notes in a map for instant deletion, editing, etc. No find required.
+type Notes = {
+  [idx: number]: Note,
+}
+
 export default function Canvas() {
+
+  const [noteCount, setNoteCount] = useState<number>(0);
+  const [notes, setNotes] = useState<Notes>({});
+
+  const addNewNote = () => {
+
+    const newNoteCount = noteCount + 1;
+    const newNote = { idx: newNoteCount, text: "" };
+
+    setNoteCount(newNoteCount);
+    setNotes(curr => ({ ...curr, ...{ [newNoteCount]: newNote } }))
+  }
+
   return (
     <Wrap>
-      <p>Canvas</p>
+      {Object.values(notes).map(note => <p>{note.idx}</p>)}
+      <AddButton addNewNote={addNewNote} />
     </Wrap>
   )
 } 
--- src/lib/scenes/canvas/components/add-button.tsx
+++ src/lib/scenes/canvas/components/add-button.tsx
@@ -1,13 +1,28 @@
+import { Plus } from 'react-bootstrap-icons'
 import { styled } from "styled-components"
 
 const StyledAddButton = styled.button`
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
 
+  font-size: 64px;
+  background: black;
+  border-radius: 50px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  border: 1px solid white;
 `;
 
-export default function AddButton() {
+type Props = {
+  addNewNote: () => void,
+}
+
+export default function AddButton({ addNewNote }: Props) {
   return (
-    <StyledAddButton>
-      
+    <StyledAddButton onClick={addNewNote}>
+      <Plus />
     </StyledAddButton>
   )
 }
